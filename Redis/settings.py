f87302b906513a2ca0856b00ce1ef9b58b6da031
Pankaj Deepak Palmate <66349985+pankajpalmate@users.noreply.github.com>
2021-07-13 17:07:08 +0530

    dfgb

--- Redis/settings.py
+++ Redis/settings.py
@@ -87,7 +87,7 @@ CACHE_TTL = 60 * 1500
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/1",
+        "LOCATION": "redis://pankaj-redis.ib12kt.0001.aps1.cache.amazonaws.com",
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient"
         },
